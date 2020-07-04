61d96cc06c65893ab2a7474e8a2ba730ec8bd77a
Aleksandr Zubakin <eldoson@yandex.ru>
2020-07-04 11:53:23 +0000

    Problem set 2, the programm readability, part two number of words

--- pset2/readability.c
+++ pset2/readability.c
@@ -9,14 +9,20 @@ int main (void)
     
     //Count amount of letters
     int count_letter = 0;
+    int count_words = 1;
+    bool is_letter;
     for (int i = 0; text[i] != '\0'; i++)
     {
         if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
         {
             count_letter++;
+        } 
+        if (text[i] == 32 && ((text[i+1] >= 65 && text[i+1] <= 90) || (text[i+1] >= 97 && text[i+1] <= 122))) 
+        {
+            count_words++;
         }
     }
-    printf("Number of letters is: %i\n", count_letter);
+    printf("Number of letters is: %i\nNumber of words is: %i\n", count_letter, count_words);
     
     
 }
