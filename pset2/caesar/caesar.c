9a734dfa9b57892c5e547c206936706db0578475
Aleksandr Zubakin <eldoson@yandex.ru>
2020-07-06 07:48:23 +0000

    Problem set 2, programm caesar part of validation the key is a number

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
@@ -8,6 +11,29 @@ int main(int argc, string argv[])
     {
         printf("Success\n");
         printf("%s\n", argv[1]);
+        int i = 0;
+        bool is_num;
+        int num;
+        while (argv[1][i] != '\0') 
+        {
+            num = isdigit(argv[1][i]);
+            if (num != 0) 
+            {
+                is_num = true;
+            } else 
+            {
+                is_num = false;
+            }
+            i++;
+        }
+        if (is_num == true)
+        {
+            num = atoi(argv[1]);
+            printf("Num is %i\n", num);
+        } else 
+        {
+           printf("Usage: ./caesar 23key\n");
+        }
     }
     if (argc==1 || argc > 2)
     {
